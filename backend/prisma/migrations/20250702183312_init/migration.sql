2ee7c471c6ee30c6168d3c2d6bf83926bf49d006
Riyan Sunesara <riyansunesara@gmail.com>
2025-07-02 14:34:02 -0400

    updated the Exercise Model to include a field for gifURL

--- backend/prisma/migrations/20250702183312_init/migration.sql
+++ backend/prisma/migrations/20250702183312_init/migration.sql
@@ -66,6 +66,7 @@ CREATE TABLE "PR" (
 CREATE TABLE "Exercise" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "gifUrl" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
